5b7d863f10190131ca4d1b3210eda6fb24aec53d
dipeshpatil <thedipeshpatil@gmail.com>
2019-10-02 17:07:25 +0530

    Second Commit Updated to Version 2.0

--- app/src/main/java/com/thedipeshpatil/blooddonationsys/MainActivity.java
+++ app/src/main/java/com/thedipeshpatil/blooddonationsys/MainActivity.java
@@ -2,155 +2,28 @@ package com.thedipeshpatil.blooddonationsys;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlertDialog;
-import android.database.Cursor;
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.Toast;
+import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+import maes.tech.intentanim.CustomIntent;
 
-    DatabaseHelper databaseHelper;
-    EditText name, email, phone, blood, id;
-    Button insert, update, delete, view;
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        databaseHelper = new DatabaseHelper(this);
-
-        id = findViewById(R.id.donor_id);
-        name = findViewById(R.id.donor_name);
-        email = findViewById(R.id.donor_email);
-        phone = findViewById(R.id.donor_phone);
-
-        insert = findViewById(R.id.insert);
-        view = findViewById(R.id.view);
-        update = findViewById(R.id.update);
-        delete = findViewById(R.id.delete);
-
-        final Spinner blood_group_spinner = findViewById(R.id.spinner_bg);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.blood_groups,R.layout.spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        blood_group_spinner.setAdapter(adapter);
-        blood_group_spinner.setOnItemSelectedListener(this);
-
-        insert.setOnClickListener(new View.OnClickListener() {
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
-                String d_name = name.getText().toString();
-                String d_email = email.getText().toString();
-                String d_phone = phone.getText().toString();
-                String d_blood = blood_group_spinner.getSelectedItem().toString();
-
-                if( !TextUtils.isEmpty(d_blood) && !TextUtils.isEmpty(d_email) && !TextUtils.isEmpty(d_name) && !TextUtils.isEmpty(d_phone)){
-                    boolean isInserted = databaseHelper.insertData(d_name,d_email,d_phone,d_blood);
-                    if (isInserted)
-                        Toast.makeText(MainActivity.this, "Data Inserted Successfully", Toast.LENGTH_LONG).show();
-                    else
-                        Toast.makeText(MainActivity.this, "Insertion Error", Toast.LENGTH_LONG).show();
-                    name.setText("");
-                    email.setText("");
-                    phone.setText("");
-                }else{
-                    Toast.makeText(MainActivity.this, "Required Fields Cannot Be Blank", Toast.LENGTH_LONG).show();
-                }
+            public void run() {
+                Intent bloodIntent = new Intent(MainActivity.this, BloodActivity.class);
+                startActivity(bloodIntent);
+                CustomIntent.customType(MainActivity.this, "right-to-left");
+                finish();
             }
-        });
-
-        view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Cursor result = databaseHelper.getAllData();
-                if(result.getCount() == 0){
-                    showMessage("Error", "No Data");
-                    return;
-                }
-                StringBuffer stringBuffer = new StringBuffer();
-                while(result.moveToNext()){
-                    stringBuffer.append("ID: " + result.getString(0) + "\n");
-                    stringBuffer.append("Name: " + result.getString(1) + "\n");
-                    stringBuffer.append("Email: " + result.getString(2) + "\n");
-                    stringBuffer.append("Phone: " + result.getString(3) + "\n");
-                    stringBuffer.append("Blood Group: " + result.getString(4) + "\n");
-                    stringBuffer.append("\n");
-                }
-                showMessage("Blood Records", stringBuffer.toString());
-            }
-        });
-
-        delete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String dID = id.getText().toString();
-                if (!TextUtils.isEmpty(dID)){
-                    boolean isDeleted = databaseHelper.deleteData(dID);
-                    if(isDeleted){
-                        Toast.makeText(MainActivity.this, "Data Deleted Successfully", Toast.LENGTH_LONG).show();
-                    }else{
-                        Toast.makeText(MainActivity.this, "Delete Error!", Toast.LENGTH_LONG).show();
-                    }
-                    name.setText("");
-                    email.setText("");
-                    phone.setText("");
-                    id.setText("");
-                }else{
-                    Toast.makeText(MainActivity.this, "Required Fields Cannot Be Blank", Toast.LENGTH_LONG).show();
-                }
-            }
-        });
-
-        update.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String uID = id.getText().toString();
-                if (!TextUtils.isEmpty(uID)){
-                    String u_name = name.getText().toString();
-                    String u_email = email.getText().toString();
-                    String u_phone = phone.getText().toString();
-                    String u_blood = blood_group_spinner.getSelectedItem().toString();
-
-                    boolean isUpdate = databaseHelper.updateData(uID, u_name, u_email, u_phone, u_blood);
-
-                    if(isUpdate){
-                        Toast.makeText(MainActivity.this, "Data Updated Successfully", Toast.LENGTH_LONG).show();
-                    }else{
-                        Toast.makeText(MainActivity.this, "Update Error!", Toast.LENGTH_LONG).show();
-                    }
-                    name.setText("");
-                    email.setText("");
-                    phone.setText("");
-                    id.setText("");
-                }else{
-                    Toast.makeText(MainActivity.this, "Required Fields Cannot Be Blank", Toast.LENGTH_LONG).show();
-                }
-            }
-        });
+        },SPLASH_TIME_OUT);
     }
-
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
-
-    }
-
-    public void showMessage(String title, String message){
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setCancelable(true);
-        builder.setTitle(title);
-        builder.setMessage(message);
-        builder.show();
-    }
-
 }
